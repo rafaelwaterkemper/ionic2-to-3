b9c875bdadd0dcffceed8e7e0de9b048b071df8e
Rafael Waterkemper <rafaelwaterkemper@gmail.com>
2018-02-06 12:50:53 -0200

    Criado os domínios de carro e acessório

--- src/pages/escolha/escolha.ts
+++ src/pages/escolha/escolha.ts
@@ -1,21 +1,40 @@
 import { Component } from '@angular/core';
 import { NavParams } from 'ionic-angular'
+import { Carro } from '../../domains/carro/carro';
+import { Acessorio } from '../../domains/carro/acessorio';
 
 @Component({
     templateUrl: './escolha.html'
 })
 export class EscolhaPage {
 
-    public carro;
+    public carro: Carro;
 
-    public acessorios;
+    public acessorios: Acessorio[];
+
+    private _precoTotal: number;
 
     constructor(public navParams: NavParams){
+        
         this.carro = this.navParams.get('carroSelecionado');
+        
+        this._precoTotal= this.carro.preco;
 
         this.acessorios = [
-            {nome: "Freio ABS", preco: 150},
-            {nome: "Câmera de ré", preco: 250}
+            new Acessorio('Freio ABS', 150),
+            new Acessorio('Câmera de ré', 250),
+            {nome: "Freio ABS 2", preco: 150},
+            {nome: "Câmera de ré 2", preco: 250}
         ]
+    }
+
+    get precoTotal(){
+        return this._precoTotal;
+    }
+
+    atualizaTotal(ligado: boolean, acessorio){
+        ligado ?
+            this._precoTotal+= acessorio.preco :
+            this._precoTotal-= acessorio.preco;
     }
 }
