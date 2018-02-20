66323bd38e35b4cc5797a2593bac8bdb61d4eca8
Rafael Waterkemper <rafaelwaterkemper@gmail.com>
2018-02-20 12:44:25 -0300

    Adequado a tela de cadastro

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -9,10 +9,19 @@ export class CadastroPage {
 
   public carro: Carro;
   public precoTotal: number;
+
+  public nome: string;
+  public endereco: string;
+  public data: string = new Date().toISOString();
+  public email: string;
   
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.carro = navParams.get('carro');
     this.precoTotal = navParams.get('precoTotal');
+  }
+
+  agendar(){
+    console.log('agendou.');
   }
 
 }
